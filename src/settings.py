ecc1ab87cbc5baab30c510f96d605f489d2ecf19
iakov <iakov2393@gmail.com>
2026-06-02 21:39:52 +0300

    .

--- src/settings.py
+++ src/settings.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
-    database_url: str = "postgresql+asyncpg://postgres:postgres@localhost:5434/ads_db"
+    postgres_connection_string: str = "postgresql+asyncpg://postgres:postgres@localhost:5434/ads_db"
     jwt_secret: str = "change-me"
     jwt_algorithm: str = "HS256"
     kafka_bootstrap_servers: str = "localhost:9092"
